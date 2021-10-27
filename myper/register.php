c7516f998652d844faa3ece9d998f0fa377138d8
GoldenFayv <69316386+GoldenFayv@users.noreply.github.com>
2021-10-27 01:32:05 +0100

    Update register.php

--- myper/register.php
+++ myper/register.php
@@ -90,12 +90,7 @@
         <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
         <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
+        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="js/jquery.js"></script>
 
@@ -118,7 +113,6 @@
                         <h2 class="intro-text text-center">Registration <strong>form</strong></h2>
                         <!-- <div id="add_err2"></div> -->
                         <hr>
-                        <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, vitae, distinctio, possimus repudiandae cupiditate ipsum excepturi dicta neque eaque voluptates tempora veniam esse earum sapiente optio deleniti consequuntur eos voluptatem.</p> -->
 
 
                         <form method="post">
@@ -168,10 +162,7 @@
                                         ?>
                                     </span>
                                 </div>
-                                <!-- <div class="form-group col-lg-12">
-                                    <label>Check password</label>
-                                    <input type="password" id="cpassword" name="cpassword" maxlength="10" class="form-control">
-                                </div> -->
+                                
                                 <div class="form-group col-lg-12">
                                     <!-- <input type="hidden" name="save" value="contact"> -->
                                     <button type="submit" class="btn btn-default" name="register" id="register">Sign Up</button>
@@ -204,56 +195,4 @@
     </body>
 
 
-
-    <!-- <script type="text/javascript">
-        $(document).ready(function(){
-            $("#register").click(function(){
-                var fname = $("#fname").val();
-                var lname = $("#lname").val();
-                var email = $("#email").val();
-                var password = $("#password").val();
-
-                $.ajax({
-                    type: "POST",
-                    url: "adduser.php",
-                    // dataType: "json",
-                    data: {fname: fname, lname: lname, email: email, password: password},
-                    success: function(output){
-                        if(output == 'true'){
-                            $("#add_err2").html('<div class="alert alert-success"><strong>Account </strong>Processed.</div>');
-                            window.location.href = "index.php";
-                        }
-                        else if(output == 'false'){
-                            $("#add_err2").html('<div class="alert alert-success"><strong>Email Address </strong>already exist.</div>');
-                        }
-                        else if(output == 'fname'){
-                            $("#add_err2").html('<div class="alert alert-success"><strong>First Name </strong>is required.</div>');
-                        }
-                        else if(output == 'lname'){
-                            $("#add_err2").html('<div class="alert alert-success"><strong>Last Name </strong>is required.</div>');
-                        }
-                        else if(output == 'eshort'){
-                            $("#add_err2").html('<div class="alert alert-success"><strong>Email address </strong>is required.</div>');
-                        }
-                        else if(output == 'pshort'){
-                            $("#add_err2").html('<div class="alert alert-success"><strong>Password </strong>must be at least 4 characters.</div>');
-                        }
-                        else if(output == 'eformat'){
-                            $("#add_err2").html('<div class="alert alert-success">Email address <strong>format is not valid </strong></div>');
-                        }
-
-                        else{
-                            $("#add_err2").html('<div class="alert alert-danger">Error processing request.<strong> Please try again</strong></div>');
-                        }
-                        
-                    },
-
-                    beforeSend: function(){
-                        $("#add_err2").html("Loading...");
-                    } 
-                });
-                return false;
-            });
-        });
-    </script> -->
 </html>
